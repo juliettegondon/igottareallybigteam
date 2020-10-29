866c0615b0f1199daa8576b0179cc6fe7e548b15
Juliette Gondon <juliettegondon@gmail.com>
2020-10-28 21:42:49 -0400

    addec functions to add employee, role, dept

--- employeeDBconnection.js
+++ employeeDBconnection.js
@@ -1,7 +1,17 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
 let cTable = require("console.table");
+var figlet = require("figlet");
 
+//fun welcome message hehe
+figlet('WELCOME', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -19,17 +29,17 @@ connection.connect((err) => {
     init();
  });
 
-//use inquirer to prompt user
+//use inquirer to prompt user, switch statement to call functions to view or add info to db
 function init(){
     inquirer.prompt({
     type: "list",
     name: "init",
     message: "What would you like to do",
-    choices: ["View All Employees", "View All Departments", "View All Roles", "Quit Application",
-    'Add Employee','Add Department', 'Add Role', 'Update Employee Role']
+    choices: ["View All Employees", "View All Departments", "View All Roles",
+    'Add Employee','Add Department', 'Add Role', 'Update Employee Role', "Quit Application"]
+
 }).then((answer) => {
     switch (answer.init) { 
-        //add breaks when done writing functions
         case "View All Employees":
                 console.log("VIEW EMPLOYEES:");
                 viewEmployees();
@@ -64,7 +74,7 @@ function init(){
              break;
     }
 });
-// create functions for each possible inq answer
+// simple functions for each possible answer from inquirer prompts upon init();
 function viewEmployees(){
         connection.query("SELECT * FROM employee",function (err, res) {
         if (err) throw err;
@@ -87,69 +97,109 @@ function viewRoles(){
     });
 };
 
-function addEmployee(){ 
-// use inquirer.prompt to input firstname, lastname, and role. 
+// use inquirer.prompt to input employee role, first, and last name
 // setup a mysql connection.query to insert info into employee table 
+function addEmployee(){ 
 inquirer.prompt([
     {
-    type: "input",
-    name: "addEmployeeRole",
-    message: "What is the employee's role?"},
+    type: "number",
+    name: "employeeRole",
+    message: "What is the employee's role ID?"},
     {
     type: "input",
-    name: "firstname",
+    name: "firstName",
     message: "What is the employee's first name?"},
     {
     type: "input",
-    name: "lastname",
+    name: "lastName",
     message: "What is the employee's last name?"}
-]);
+]).then((answer) => {
+    connection.query(
+        "INSERT INTO employee SET ?", {
+        role_id: answer.employeeRole,
+        first_name: answer.firstName,
+        last_name: answer.lastName},
+        function (err) {
+            if (err) throw err;
+            console.log ("ADDED EMPLOYEE ---------")
+            console.table(answer);
+            init();
+        }
+    )});
 }
-
+// use inquirer.prompt to input department name
+// setup a mysql connection.query to insert info into department table 
 function addDepartment(){ 
-inquirer.prompt({
-    type: "input",
+inquirer.prompt([
+    {type: "input",
     name: "addDepartmentName",
-    message: "What is the new department called?"
-});
-}// setup a mysql connection.query to insert info into department table 
-
-function addDepartment(){ 
-inquirer.prompt({
-    type: "input",
-    name: "addDepartmentName",
-    message: "What is the new department called?"
-});
-}// setup a mysql connection.query to insert info into department table 
-
+    message: "What is the new department called?"},
+    ]).then((answer) => {
+        connection.query(
+        "INSERT INTO department SET ?", {
+            departmentname: answer.addDepartmentName},
+            function (err) {
+                if (err) throw err;
+                console.log ("ADDED EMPLOYEE ---------")
+                console.table(answer);
+                init();
+            }
+        )});
+}
+// use inquirer.prompt to input role title and salary
+// setup a mysql connection.query to insert info into role table 
 function addRole(){
-inquirer.prompt({
-    type: "input",
-    name: "addtitle",
+inquirer.prompt([
+    {type: "input",
+    name: "addTitle",
     message: "What is the new role called?"},
     {
-    type: "input",
-    name: "addsalary",
-    message: "What is the salary of this role?"
-});
-}// setup a mysql connection.query to insert info into role table 
+    type: "number",
+    name: "addSalary",
+    message: "What is the salary of this role?"}
+    ]).then((answer) => {
+        connection.query(
+            "INSERT INTO role SET ?", {
+            title: answer.addTitle,
+            salary: answer.addSalary},
+            function (err) {
+                if (err) throw err;
+                console.log ("ADDED ROLE ---------")
+                console.table(answer);
+                init();
+            }
+        )}
+    );
+}
 
- 
-// function update();
-//use inquirer.prompt to have user select employee name, return employee and role . 
+ //use inquirer.prompt to have user select employee name, return employee and role . 
 // setup a mysql connection.query to insert info into role ID 
-/* inquirer.prompt({
+
+/* OK WHERE IM CURRENTLY AT... TRYING TO FIGURE OUT HOW TO GET THE CURRENT TABLE OF EMPLOYEES
+INTO AN EMPTY ARRAY TO PRESENT AS CHOICES IN THE PROMPT... THINKING  
+A FOR LOOP AND ITERATE THROUGH THE ARRAY
+ */
+function update() {
+    const employeesArray = [];
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+        console.log( "----- UPDATING IN PROGRESS ------")
+        console.table(res);
+    });
+}
+inquirer.prompt([{
     type: "list",
     name: "updateEmployee",
-    message: "Which employee would you like to update?"}
-    choices: //// VIEW ALL EMPLOYEES FUNCTION,
-    {
+    message: "Which employee would you like to update?",
+    choices:["CALL ARRAY HERE"]
+},
+{
     type: "list",
     name: "updateName",
-    message: "Are you changing the first name, last name, or role?"
+    message: "Are you changing the first name, last name, or role?",
     choices: ["First Name", "Last Name", "Role"]
-};) */
-
+}
+])
 // end of connection to db
 //connection.end();
 }
